f453654433c5a0a2461232f53a13fa5bbad3d89c
Cassio7 <acassieri@gmail.com>
2024-05-10 02:07:16 +0200

    lettura da file ed altri cambiamenti

--- src/Main.java
+++ src/Main.java
@@ -1,93 +1,64 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 import java.util.Vector;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-class FinestraPrinc implements ActionListener {
-    static int W = 700;
-    JFrame f;
-    JTable t;
-    JButton nuovo, modifica, elimina;
+class Utente {
 
-    FinestraPrinc(Vector<Persona> temp) {
-        f = new JFrame("Rubrica");
-        // titoli colonne
-        String[] colonna = {"Nome", "Cognome", "Numero"};
-        // crea la tabella ed inserisce come righe le informazioni prese dalla funzione
-        t = new JTable(getData(temp), colonna);
-        t.setBounds(30, 40, 200, 300);
-        JScrollPane sp = new JScrollPane(t);
-        f.add(sp);
+    String username;
 
-        JToolBar toolBar = new JToolBar();
-        toolBar.setLayout(new FlowLayout(FlowLayout.CENTER));
-        // creazione dei 3 bottoni
-        createButtons();
-
-        // aggiungo i bottoni alla toolbar
-        toolBar.add(nuovo);
-        toolBar.add(modifica);
-        toolBar.add(elimina);
-
-        // aggiungo la toolbar al JFrame
-        f.add(toolBar, BorderLayout.SOUTH);
-
-        // imposto finestra
-        f.setSize(W, 500);
-        f.setLocationRelativeTo(null);
-        f.setVisible(true);
-        // chiude finestra ed interrompe applicazione
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    Utente(String username) {
+        this.username = username;
     }
 
-    // prende tutte le info dal vettore e le inserisce in una matrice di stringhe che poi restituisce
-    String[][] getData(Vector<Persona> temp) {
-        String[][] data = new String[temp.size()][3];
-        for (int i = 0; i < temp.size(); i++) {
-            data[i][0] = temp.get(i).nome;
-            data[i][1] = temp.get(i).cognome;
-            data[i][2] = temp.get(i).telefono;
+    // prende tutti i file dentro la cartella informazioni e crea una lista
+    void getFiles(Vector<Persona> temp) {
+        File cartella = new File("src/informazioni/");
+        List<File> fileList = Arrays.asList(cartella.listFiles());
+        try {
+            // scorro tutti i file
+            for (int i = 0; i < fileList.size(); i++) {
+                // classe per leggere da file
+                Scanner reader = new Scanner(fileList.get(i));
+                // leggo finche sono presenti righe
+                while (reader.hasNextLine()) {
+                    // inserisco la riga, separandola tramite la virgola, dentro persona, eta ha un controllo sullo spazio per eliminare errore casting
+                    String[] data = reader.nextLine().split(",");
+                    temp.add(new Persona(data[0], data[1], data[2], data[3], Integer.parseInt(data[4].replaceAll("\\s+", ""))));
+                }
+                // chiudo lettura
+                reader.close();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
         }
-        return data;
     }
 
-    private void createButtons() {
-        // definisco bottoni
-        Icon add = new ImageIcon("src/img/add.png");
-        Icon edit = new ImageIcon("src/img/edit.png");
-        Icon remove = new ImageIcon("src/img/remove.png");
-        nuovo = new JButton(add);
-        modifica = new JButton(edit);
-        elimina = new JButton(remove);
-        // imposto grandezza
-        nuovo.setPreferredSize(new Dimension(60, 50));
-        modifica.setPreferredSize(new Dimension(60, 50));
-        elimina.setPreferredSize(new Dimension(60, 50));
-        // aggiungo listener per eventi che punta a se stesso
-        nuovo.addActionListener(this);
-        modifica.addActionListener(this);
-        elimina.addActionListener(this);
-    }
-
-    // controlli onclick eventi bottoni
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == nuovo) {
-            new FinestraNuovo();
-        } else if (e.getSource() == modifica) {
-            System.out.println("modifica");
-        } else if (e.getSource() == elimina) {
-            System.out.println("elimina");
-        }
-
-    }
 }
+
 
 
 public class Main {
     public static void main(String[] args) {
+
+        Login login = new Login();
+        while (true) {
+            if (login.isAccesso())
+                break;
+
+        }
         Vector<Persona> persone = new Vector<>();
-        persone.add(new Persona("Giorgio", "Verdi", "3334414987", "Via Aranciotto 77", 23));
+        Utente utente = new Utente(login.getUser());
+        //persone.add(new Persona("Giorgio", "Verdi", "3334414987", "Via Aranciotto 77", 23));
+        utente.getFiles(persone);
         new FinestraPrinc(persone);
+
     }
+
+
 }
