15893ea8d40c07b5811807acb4e07a70ba2daca2
Cassio7 <acassieri@gmail.com>
2024-05-10 18:57:27 +0200

    refresh della pagina al cambiamento

--- src/Main.java
+++ src/Main.java
@@ -8,38 +8,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-class Utente {
 
-    String username;
-
-    Utente(String username) {
-        this.username = username;
-    }
-
-    // prende tutti i file dentro la cartella informazioni e crea una lista
-    void getFiles(Vector<Persona> temp) {
-        File cartella = new File("src/informazioni/");
-        List<File> fileList = Arrays.asList(cartella.listFiles());
-        try {
-            // scorro tutti i file
-            for (int i = 0; i < fileList.size(); i++) {
-                // classe per leggere da file
-                Scanner reader = new Scanner(fileList.get(i));
-                // leggo finche sono presenti righe
-                while (reader.hasNextLine()) {
-                    // inserisco la riga, separandola tramite la virgola, dentro persona, eta ha un controllo sullo spazio per eliminare errore casting
-                    String[] data = reader.nextLine().split(",");
-                    temp.add(new Persona(data[0], data[1], data[2], data[3], Integer.parseInt(data[4].replaceAll("\\s+", ""))));
-                }
-                // chiudo lettura
-                reader.close();
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println(e);
-        }
-    }
-
-}
 
 
 
@@ -55,9 +24,16 @@ public class Main {
         Vector<Persona> persone = new Vector<>();
         Utente utente = new Utente(login.getUser());
         //persone.add(new Persona("Giorgio", "Verdi", "3334414987", "Via Aranciotto 77", 23));
-        utente.getFiles(persone);
-        new FinestraPrinc(persone);
+        utente.getData(persone);
+        FinestraPrinc finestraPrinc = new FinestraPrinc(persone,utente);
+        while (true) {
+            if (finestraPrinc.getRefresh() || FinestraPrinc.refreshforced){
+                persone = new Vector<>();
+                utente.getData(persone);
+                finestraPrinc = new FinestraPrinc(persone,utente);
+            }
 
+        }
     }
 
 
